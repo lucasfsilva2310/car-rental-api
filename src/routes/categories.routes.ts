bf17b914d8fd14d7d5c47d51c3a70ded5afdad02
lucas <lucas.f.silva@provi.com.br>
2022-08-19 19:50:17 -0300

    creating new route to upload categories via csv, adding multer library to handle file import

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,16 @@
+// library to deal with file imports
+import multer from 'multer'
+
 import { Router, Request, Response } from 'express'
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { getAllCategoriesController } from '../modules/cars/useCases/getAllCategories'
 
 const categoriesRoutes = Router()
+const uploadFile = multer({
+  // files destiny
+  dest: './tmp'
+})
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response)
@@ -11,5 +19,17 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
 categoriesRoutes.get('/', (request: Request, response: Response) => {
   return getAllCategoriesController.handle(request, response)
 })
+
+categoriesRoutes.post(
+  '/upload',
+  uploadFile.single('categories'),
+  (request: Request, response: Response) => {
+    const { file } = request
+    console.log(file)
+    return response
+      .status(201)
+      .json({ status: 'success', message: 'File uploaded.' })
+  }
+)
 
 export { categoriesRoutes }
