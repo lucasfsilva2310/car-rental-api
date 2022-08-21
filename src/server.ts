ba52c23bf412867c951a3dfbcfeb9d4567c13501
lucas <lucas.f.silva@provi.com.br>
2022-08-21 14:35:04 -0300

    installing swagger-ui-express lib for API documentation and adding both create and getAll from categories and specifications, just missing upload endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerFile from './swagger.json'
 import { loadRoutes } from './routes/index.routes'
 
 const app = express()
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(express.json())
 
 loadRoutes(app)
