2cfc62c80a4cb1848c1b00027278cf94bc8fbecc
lucas <lucas.f.silva@provi.com.br>
2022-08-22 22:09:43 -0300

    configuring Dockerfile and docker-compose to build both database and application, creating an index.ts file inside database with typeorm config to initialize and connect database, creating new script inside package.json to create migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,30 @@
-import { createConnection, getConnectionOptions } from 'typeorm'
+import { DataSource } from 'typeorm'
 
-interface IOptions {
-  host: string
-}
-
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions
-  newOptions.host = 'car_rental' // Database name as it is on ormconfig.json
-  createConnection({
-    ...options
-  })
+const dbConnection = new DataSource({
+  migrationsTableName: 'migrations',
+  type: 'postgres',
+  host: 'db',
+  username: 'docker',
+  password: 'docker',
+  port: 5432,
+  database: 'dbcar_rental',
+  logging: false,
+  synchronize: false,
+  name: 'default',
+  migrations: ['src/database/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscriber/**/*{.ts,.js}'],
+  entities: ['src/**/**.entity{.ts,.js}']
 })
 
-createConnection()
+dbConnection
+  .initialize()
+  .then(() => console.log('Postgres database connected successfully.'))
+  .catch((error) =>
+    console.log('Error when trying to connect with postgres database', error)
+  )
+
+export { dbConnection }
+// To Create new migrations:
+// npm run typeorm migration:create -n <migration_name>
+
+// Find out why migrations are being created in root
